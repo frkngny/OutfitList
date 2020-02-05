a9edfa184a91bc4eb1b728137bcb6f3a02022069
frkngny <frkang35@gmail.com>
2020-02-05 23:38:02 +0300

    using tflite model in android (Upgraded version)

--- android_tflite/app/src/main/java/com/example/tflitetry/MainActivity.java
+++ android_tflite/app/src/main/java/com/example/tflitetry/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.tflitetry;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
-import android.graphics.ColorSpace;
-import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,57 +17,50 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.tensorflow.lite.Interpreter;
-import org.tensorflow.lite.support.model.Model;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.Arrays;
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    TextView result_tv;
+    Button btnCamera, btnGallery, btnClassify;
     private static final int PICK_IMAGE = 1;
     Uri imageUri;
-    Bitmap bitmap, bitmap1;
-    private Executor executor = Executors.newSingleThreadExecutor();
-    private Classifier classifier;
-    String modelFile = "fashion_model.tflite";
-    Interpreter tflite;
+    Bitmap bitmap;
+    private static final String modelFile = "converted.tflite";
+    private Interpreter tflite;
+    ByteBuffer buffer;
 
-    private static final int MAX_RESULTS = 3;
-    private static final int BATCH_SIZE = 1;
-    private static final int PIXEL_SIZE = 3;
-    private static final float THRESHOLD = 0.1f;
-
-    private int inputSize;
-    private List<String> labelList;
-
-    private static final int IMAGE_MEAN = 128;
-    private static final float IMAGE_STD = 128.0f;
-    private boolean quant;
-
+    /** fashion_model.tflite and converted.tflite are both the same.
+     * Only one difference, converted.tflite has slightly more accuracy.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnCamera = findViewById(R.id.btnCamera);
+        btnCamera = findViewById(R.id.btnCamera);
         imageView = findViewById(R.id.imageView);
-        Button btnGallery = findViewById(R.id.btnGallery);
-        Button btnClassify = findViewById(R.id.btnClassify);
-        TextView result_tv = findViewById(R.id.result_tv);
+        btnGallery = findViewById(R.id.btnGallery);
+        btnClassify = findViewById(R.id.btnClassify);
+        result_tv = findViewById(R.id.result_tv);
+
+    }
+
+    public void btnClassifyClicked(View v){
+        float[][] out = new float[1][10];
+        int bytes = bitmap.getByteCount();
+        buffer = ByteBuffer.allocate(bytes);
+        bitmap.copyPixelsToBuffer(buffer);
 
         try {
             tflite = new Interpreter(loadModelFile(this, modelFile));
@@ -78,59 +69,36 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+        tflite.run(buffer, out);
+
+        for(int i = 0; i < 10; i++) {
+            Log.e("number", String.valueOf(out[0][i]));
+            if (out[0][i] <= 0.5) {
+                out[0][i] = 0;
+            } else {
+                out[0][i] = 1;
             }
-        });
+        }
 
-        btnGallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
-            }
-        });
-
-        btnClassify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //initTensorFlowAndLoadModel();
-                /*
-                Intent i = new Intent(MainActivity.this, TakeImage.class);
-                ByteArrayOutputStream bs = new ByteArrayOutputStream();
-                bitmap1.compress(Bitmap.CompressFormat.PNG, 50, bs);
-                i.putExtra("byteArray", bs.toByteArray());
-                startActivity(i);
-                 */
-
-                //byte [] out = new byte[]{0};
-
-                //ByteArrayOutputStream bs = new ByteArrayOutputStream();
-                //bitmap1.compress(Bitmap.CompressFormat.PNG, 50, bs);
-
-                ByteBuffer byteBuffer = convertBitmapToByteBuffer(bitmap1);
-
-                if(quant){
-                    byte[][] result = new byte[1][10];
-                    tflite.run(byteBuffer, result);
-                    result_tv.setText(String.valueOf(getSortedResultByte(result)));
-                } else {
-                    float [][] result = new float[1][10];
-                    tflite.run(byteBuffer, result);
-                    result_tv.setText(String.valueOf(getSortedResultFloat(result)));
-                }
-
-                //tflite.run(byteBuffer,out);
-                //result_tv.setText(String.valueOf(Math.round(out[0])));
-            }
-        });
+        result_tv.setText(Arrays.deepToString(out));
     }
+
+    public void btnGalleryClicked(View v){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+
+    public void btnCameraClicked(View v){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+
 
     private MappedByteBuffer loadModelFile(Activity activity, String MODEL_FILE) throws IOException {
         AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(MODEL_FILE);
@@ -148,114 +116,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         if (requestCode==0 && resultCode == RESULT_OK){
-            bitmap = (Bitmap) data.getExtras().get("data");
+            assert data != null;
+            bitmap = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
             imageView.setImageBitmap(bitmap);
         }
 
         if (requestCode==PICK_IMAGE && resultCode == RESULT_OK){
+            assert data != null;
             imageUri = data.getData();
             try{
-                bitmap1 = MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
-                imageView.setImageBitmap(bitmap1);
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
+                imageView.setImageBitmap(bitmap);
             }
             catch (IOException e) {
                 e.printStackTrace();
             }
         }
-    }
-
-    private ByteBuffer convertBitmapToByteBuffer(Bitmap bitmap) {
-        ByteBuffer byteBuffer;
-
-        if(quant) {
-            byteBuffer = ByteBuffer.allocateDirect(BATCH_SIZE * inputSize * inputSize * PIXEL_SIZE);
-        } else {
-            byteBuffer = ByteBuffer.allocateDirect(4 * BATCH_SIZE * inputSize * inputSize * PIXEL_SIZE);
-        }
-
-        byteBuffer.order(ByteOrder.nativeOrder());
-        int[] intValues = new int[inputSize * inputSize];
-        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-        int pixel = 0;
-        for (int i = 0; i < inputSize; ++i) {
-            for (int j = 0; j < inputSize; ++j) {
-                final int val = intValues[pixel++];
-                if(quant){
-                    byteBuffer.put((byte) ((val >> 16) & 0xFF));
-                    byteBuffer.put((byte) ((val >> 8) & 0xFF));
-                    byteBuffer.put((byte) (val & 0xFF));
-                } else {
-                    byteBuffer.putFloat((((val >> 16) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
-                    byteBuffer.putFloat((((val >> 8) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
-                    byteBuffer.putFloat((((val) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
-                }
-
-            }
-        }
-        return byteBuffer;
-    }
-
-
-    @SuppressLint("DefaultLocale")
-    private List<Classifier.Recognition> getSortedResultByte(byte[][] labelProbArray) {
-
-        PriorityQueue<Classifier.Recognition> pq =
-                new PriorityQueue<>(
-                        MAX_RESULTS,
-                        new Comparator<Classifier.Recognition>() {
-                            @Override
-                            public int compare(Classifier.Recognition lhs, Classifier.Recognition rhs) {
-                                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
-                            }
-                        });
-
-        for (int i = 0; i < labelList.size(); ++i) {
-            float confidence = (labelProbArray[0][i] & 0xff) / 255.0f;
-            if (confidence > THRESHOLD) {
-                pq.add(new Classifier.Recognition("" + i,
-                        labelList.size() > i ? labelList.get(i) : "unknown",
-                        confidence, quant));
-            }
-        }
-
-        final ArrayList<Classifier.Recognition> recognitions = new ArrayList<>();
-        int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
-        for (int i = 0; i < recognitionsSize; ++i) {
-            recognitions.add(pq.poll());
-        }
-
-        return recognitions;
-    }
-
-    @SuppressLint("DefaultLocale")
-    private List<Classifier.Recognition> getSortedResultFloat(float[][] labelProbArray) {
-
-        PriorityQueue<Classifier.Recognition> pq =
-                new PriorityQueue<>(
-                        MAX_RESULTS,
-                        new Comparator<Classifier.Recognition>() {
-                            @Override
-                            public int compare(Classifier.Recognition lhs, Classifier.Recognition rhs) {
-                                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
-                            }
-                        });
-
-        for (int i = 0; i < labelList.size(); ++i) {
-            float confidence = labelProbArray[0][i];
-            if (confidence > THRESHOLD) {
-                pq.add(new Classifier.Recognition("" + i,
-                        labelList.size() > i ? labelList.get(i) : "unknown",
-                        confidence, quant));
-            }
-        }
-
-        final ArrayList<Classifier.Recognition> recognitions = new ArrayList<>();
-        int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
-        for (int i = 0; i < recognitionsSize; ++i) {
-            recognitions.add(pq.poll());
-        }
-
-        return recognitions;
     }
 
 
